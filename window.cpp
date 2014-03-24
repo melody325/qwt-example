ce6c5861144dc75a901e7cc1b6e55219b8f79ea9
melody325 <ljmerry@qq.com>
2014-03-24 13:47:02 +0000

    window.cpp

--- window.cpp
+++ window.cpp
@@ -1,6 +1,6 @@
 #include "window.h"
 #include <gpio-sysfs.h>
-#include "adcreader.h"
+#include "adcreader.cpp"
 
 #include <cmath>  // for sine stuff
 
@@ -56,8 +56,8 @@ Window::Window() : gain(5), count(0)
 	// At the moment it doesn't do anything else than
 	// running in an endless loop and which prints out "tick"
 	// every second.
-	adcreader = new ADCreader();
-	adcreader->start();
+	//adcreader = new ADCreader();
+	//0adcreader->start();
 }
 
 Window::~Window() {
