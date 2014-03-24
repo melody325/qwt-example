c58cadfd1c0093c57ad1794d23382eac34e3edc3
melody325 <ljmerry@qq.com>
2014-03-24 18:21:41 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,10 +10,10 @@ int main(int argc, char *argv[])
 	QApplication app(argc, argv);
 	
 	//add the thread
-        adcreader = new ADCreader();
-        adcreader->start();
-        if (adcreader->has Sample())
-            inVal = adcreader->getSample();
+        //adcreader = new ADCreader();
+        //adcreader->start();
+        //if (adcreader->has Sample())
+            //inVal = adcreader->getSample();
             
 	// create the window
 	Window window;
