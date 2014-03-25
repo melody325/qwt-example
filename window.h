383bf192509aef470d49a383257905cb83045a4b
melody325 <ljmerry@qq.com>
2014-03-25 19:22:26 +0000

    Update window.h

--- window.h
+++ window.h
@@ -11,6 +11,8 @@
 #include "adcreader.h"
 
 // class definition 'ADCreader'
+class ADCreader ;
+
 	  
 
 // class definition 'Window'
@@ -20,6 +22,8 @@ class Window : public QWidget
 	Q_OBJECT
 
 public:
+        ADCreader *adcreader;
+        adcreader = new ADCreader();
 	Window(); // default constructor - called when a Window is declared without arguments
 
 	~Window();
@@ -49,7 +53,7 @@ private:
 	double gain;
 	int count;
 
-	ADCreader *adcreader;
+	//ADCreader *adcreader;
 };
 
 #endif // WINDOW_H
